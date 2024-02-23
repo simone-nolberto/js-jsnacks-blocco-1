ae0685e67fac6433d09ed8aeee7513250bbd138b
simone-nolberto <simone.nolberto@outlook.com>
2024-02-23 12:38:34 +0100

    add
    snack 2

---
+++ jsnack2/script2.js
@@ -0,0 +1,11 @@
+let word1 = prompt ("Scrivi una prima parola");
+let word2 = prompt ("Scrivi una seconda parola");
+
+
+// console.log(word1, word2);
+
+if (word1.length > word2.length){
+    console.log(word1, word2);
+} else {
+    console.log(word2, word1);
+}
