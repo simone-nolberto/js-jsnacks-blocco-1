c128279dc2be8f91e7a786292550853479ac4872
simone-nolberto <simone.nolberto@outlook.com>
2024-02-23 12:12:44 +0100

    initial commit
    log biggest numer

---
+++ jsnack1/main.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Inserisci un numero qualsiasi"));
+// console.log(num1);
+
+
+let num2 = Number(prompt("Inserisci un numero qualsiasi"));
+// console.log(num2);
+
+if (num1 > num2){
+    console.log(num1);
+} else {
+    console.log(num2);
+}
