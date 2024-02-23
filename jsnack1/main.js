8d06b924456ada8041968ccdd4128a72acd34926
simone-nolberto <simone.nolberto@outlook.com>
2024-02-23 12:16:00 +0100

    update

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -1,8 +1,8 @@
-let num1 = Number(prompt("Inserisci un numero qualsiasi"));
+let num1 = Number(prompt("Inserisci un primo numero qualsiasi"));
 // console.log(num1);
 
 
-let num2 = Number(prompt("Inserisci un numero qualsiasi"));
+let num2 = Number(prompt("Inserisci un secondo numero qualsiasi"));
 // console.log(num2);
 
 if (num1 > num2){
