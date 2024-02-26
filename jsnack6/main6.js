f12b663e11c5c61c81449a7431a787e1893e2cf1
simone-nolberto <simone.nolberto@outlook.com>
2024-02-26 12:31:07 +0100

    update

--- jsnack6/main6.js
+++ jsnack6/main6.js
@@ -2,18 +2,23 @@ console.log("it works!!");
 
 let num = [];
 
+// for (let i = 0; i < 6; i++) {
+//     const request = Number(prompt('Inserisci un numero'));
+//     console.log(request);
+//     num.push(request);
+    
+// }
 
+// console.log(num);
 
-
-
-
+// console.log(Math.pow(num[2],3));
 
 
 
 
 i = 0;
 
-while (i < 6) {
+while (i < 10) {
     const request = Number(prompt('Inserisci un numero'));
     
     num.push(request)
@@ -23,4 +28,7 @@ while (i < 6) {
 
 console.log(num);
 
-console.log(Math.pow(num[2],3));
+const control = Number(prompt("Specifica un numero da 1 a 10"));
+
+
+console.log(Math.pow(num[control],3));
