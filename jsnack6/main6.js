cf99c7287d873cdf0fd059eaaa7a2e8bc90420df
simone-nolberto <simone.nolberto@outlook.com>
2024-02-26 12:19:51 +0100

    add
    snack 6 (while loop)

---
+++ jsnack6/main6.js
@@ -0,0 +1,26 @@
+console.log("it works!!");
+
+let num = [];
+
+
+
+
+
+
+
+
+
+
+i = 0;
+
+while (i < 6) {
+    const request = Number(prompt('Inserisci un numero'));
+    
+    num.push(request)
+
+    i++
+}
+
+console.log(num);
+
+console.log(Math.pow(num[2],3));
