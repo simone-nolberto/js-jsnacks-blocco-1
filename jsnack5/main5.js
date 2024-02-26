71e065e22ec58d3e83d25d830dbcdb6ef31333d5
simone-nolberto <simone.nolberto@outlook.com>
2024-02-26 11:52:16 +0100

    add
    snack 5

---
+++ jsnack5/main5.js
@@ -0,0 +1,34 @@
+console.log("it works");
+
+let oddNum = [];
+
+
+
+// for (let i = 0; i < 6; i++) {
+//     const request = Number(prompt("Inserisci un numero"));
+//     console.log(request);
+
+//     if (request % 2 !== 0) {
+//         oddNum.push(request)
+//     }
+
+// }
+
+// console.log(oddNum);
+
+
+i = 0;
+
+while (i < 6) {
+    const request = Number(prompt('Inserisci un numero'));
+
+    if (request % 2 !== 0){
+        oddNum.push(request)
+    }
+
+
+
+    i++
+}
+
+console.log(oddNum);
